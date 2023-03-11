279d57ceb2bea0d26fb9541b5b33cc1603ab2cdd
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-11 13:00:23 -0300

    style: add lint and commit-hooks config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    mocha: true,
+  },
+  extends: ['plugin:react/recommended', 'google'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'require-jsdoc': ['off'],
+    'react/prop-types': ['off'],
+    'spaced-comment': ['off'],
+    'prettier/prettier': ['error', {}, {usePrettierrc: true}],
+    'react/no-children-prop': ['off'],
+    'indent': ['off'],
+    'quotes': ['off'],
+    'max-len': ['off'],
+    'operator-linebreak': [
+      'error',
+      'after',
+      {overrides: {'?': 'before', ':': 'before'}},
+    ],
+  },
+};
